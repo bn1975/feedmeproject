9164f8073bc29b1ec10c2c80506b0effe21bad0d
Brent Nicholas <brentnicholas75@gmail.com>
2018-01-22 18:09:06 -0500

    trying to get postgres to work in deployment 2

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   // OUR LOCAL SETUP FOR OUR DB
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/yelp_project'
+    connection:'postgres://kwwoyjdeeedhff:7618ecd5660020d7f68a664f4ec702b7d394f4f8d912df78d352cfe9c1913ea0@ec2-107-21-236-219.compute-1.amazonaws.com:5432/d5pk1f7mere4ov'
   },
   // OUR PRODUCTION/LIVE SETUP
   production: {}
